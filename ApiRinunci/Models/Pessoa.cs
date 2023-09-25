afffeda0d0246dd188e5d7cd5ddfc5b74d4ec9a0
Ricardo Nascimento <ricardoan287@gmail.com>
2023-09-25 11:23:20 -0300

    Criado Classe Pessoa
    Criado Classe Pessoa

---
+++ ApiRinunci/Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ApiRinunci.Models
+{
+    public class Pessoa
+    {
+
+        [Key]
+        public int pessoaId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string nomePessoa{ get; set; }
+    }
+}
