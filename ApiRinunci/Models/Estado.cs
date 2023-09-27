43aa3c9ab67033ea51ffcd16c21030b9d24a9bce
Ricardo Nascimento <ricardoan287@gmail.com>
2023-09-27 10:17:24 -0300

    criacao estado
    criacao de estado

---
+++ ApiRinunci/Models/Estado.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ApiRinunci.Models
+{
+    public class Estado
+    {
+        [Key]
+        public int estadoId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string nomeEstado { get; set; }
+    }
+}
